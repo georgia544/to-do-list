703ee78e78e69638eca7960f6e57630b914e630d
georgia544 <diana.shakh98@gmail.com>
2023-12-14 19:48:23 +0300

    update bottom navigation

---
+++ app/src/main/java/com/hfad/todolist/StarTasksFragment.kt
@@ -0,0 +1,17 @@
+package com.hfad.todolist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class StarTasksFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_star_tasks,container,false)
+    }
+}
